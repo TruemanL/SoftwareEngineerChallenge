cf92adae51e08da4bec704a903bd1bdbf0f3b5d9
TruemanL <truemanlam@gmail.com>
2019-06-18 22:59:49 -0700

    finished adding tests to queue

--- CodingTask/src/test/scala/QueueSpec.scala
+++ CodingTask/src/test/scala/QueueSpec.scala
@@ -61,10 +61,71 @@ class QueueSpec extends FunSpec with Matchers {
     }
   }
 
-//  describe("A Queue should behave as expected") {
-//    val testElements = Vector(1, 2, 3, 4, 5)
-//    val testQueue = Queue.empty[Int]
-//    it("enQueuing and deQueuing items should maintain FIFO order") {
-//    }
-//  }
+  describe("A Queue should behave as expected") {
+    val testElements = (1 to 10000).toVector
+    it("enQueuing and deQueuing items should maintain FIFO order for many items") {
+      val filledUpTestQueue = testElements.foldLeft(Queue.empty[Int]){
+        (queue, element) => queue.enQueue(element)
+      }
+      var testQueue = filledUpTestQueue
+      testQueue.isEmpty shouldBe false
+      for (element <- testElements) {
+        testQueue.head shouldBe Some(element)
+        testQueue = testQueue.deQueue()
+      }
+      testQueue.isEmpty shouldBe true
+    }
+
+    it("enQueuing and deQueuing items should maintain FIFO order for enQueues interleaved with deQueues") {
+      var testQueue = Queue.empty[Int]
+      testQueue.isEmpty shouldBe true
+      testQueue = testQueue.enQueue(1)
+      testQueue = testQueue.enQueue(2)
+      testQueue = testQueue.enQueue(3)
+
+      testQueue.head shouldBe Some(1)
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some(2)
+      testQueue = testQueue.deQueue()
+
+      testQueue = testQueue.enQueue(4)
+      testQueue = testQueue.enQueue(5)
+      testQueue.isEmpty shouldBe false
+
+      testQueue.head shouldBe Some(3)
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some(4)
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some(5)
+      testQueue = testQueue.deQueue()
+      testQueue.isEmpty shouldBe true
+    }
+  }
+
+  describe("A Queue should also work for non-integer types") {
+    it("enQueuing and deQueuing items should maintain FIFO order for enQueues interleaved with deQueues") {
+      var testQueue = Queue.empty[String]
+      testQueue.isEmpty shouldBe true
+      testQueue = testQueue.enQueue("a")
+      testQueue = testQueue.enQueue("b")
+      testQueue = testQueue.enQueue("c")
+
+      testQueue.head shouldBe Some("a")
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some("b")
+      testQueue = testQueue.deQueue()
+
+      testQueue = testQueue.enQueue("d")
+      testQueue = testQueue.enQueue("e")
+      testQueue.isEmpty shouldBe false
+
+      testQueue.head shouldBe Some("c")
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some("d")
+      testQueue = testQueue.deQueue()
+      testQueue.head shouldBe Some("e")
+      testQueue = testQueue.deQueue()
+      testQueue.isEmpty shouldBe true
+    }
+  }
 }
